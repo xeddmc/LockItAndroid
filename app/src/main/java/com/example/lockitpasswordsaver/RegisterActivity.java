efd5b9bc1cbed35d994d6192791cfa8f6503a1ae
Jorge Zapata <jorgejaden@gmail.com>
2020-02-07 21:08:47 +0100

    Second Git

--- app/src/main/java/com/example/lockitpasswordsaver/RegisterActivity.java
+++ app/src/main/java/com/example/lockitpasswordsaver/RegisterActivity.java
@@ -1,18 +1,34 @@
 package com.example.lockitpasswordsaver;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
 
     EditText mUsernameet, mEmailEt, mPasswordet;
     Button mRegisterButton;
+    ProgressDialog progressDialog;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +44,50 @@ public class RegisterActivity extends AppCompatActivity {
         mEmailEt = findViewById(R.id.email_textField);
         mPasswordet = findViewById(R.id.password_textField);
         mRegisterButton = findViewById(R.id.sign_up_button);
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Working...");
+
+        mAuth = FirebaseAuth.getInstance();
 
         mRegisterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                System.out.println("String");
+                String email = mEmailEt.getText().toString().trim();
+                String password = mPasswordet.getText().toString().trim();
+                if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    mEmailEt.setError("Invalid Email");
+                    mEmailEt.setFocusable(true);
+                } else if (password.length() < 6) {
+                    mPasswordet.setError("Please Enter A Password At Least 6 Digits");
+                    mPasswordet.setFocusable(true);
+                } else {
+                    registerUser(email, password);
+                }
+            }
+        });
+    }
+
+    private void registerUser(String email, String password) {
+        progressDialog.show();
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            progressDialog.dismiss();
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(RegisterActivity.this, "Registered", Toast.LENGTH_SHORT).show();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            progressDialog.dismiss();
+                            Toast.makeText(RegisterActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                progressDialog.dismiss();
+                Toast.makeText(RegisterActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
     }
